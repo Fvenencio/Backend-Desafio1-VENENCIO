a8febdb5ba592873c105693d5abc7992db4a56ce
Fabrizio Venencio <fvenencio@gmail.com>
2023-10-17 17:25:46 -0300

    desafio entregable 2

--- DesafioEntregable1-VENENCIO.js
+++ DesafioEntregable1-VENENCIO.js
@@ -1,73 +1,101 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
-      this.products = [];
-      this.productIdCounter = 1;
+  constructor(filePath) {
+    this.filePath = filePath;
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, '[]', 'utf8'); // Crea un archivo JSON vacío si no existe.
     }
-  
-    addProduct(product) {
-      if (this.isProductValid(product)) {
-        product.code = this.productIdCounter++;
-        this.products.push(product);
-        return product.code;
+    this.products = this.loadProducts();
+    this.productIdCounter = this.calculateNextId();
+  }
+
+  loadProducts() {
+    try {
+      if (fs.existsSync(this.filePath)) {
+        const data = fs.readFileSync(this.filePath, 'utf8');
+        return JSON.parse(data);
       }
-    }
-  
-    isProductValid(product) {
-      const requiredFields = ['title', 'description', 'price', 'thumbnail' , 'stock'];
-  
-      for (const field of requiredFields) {
-        if (!product.hasOwnProperty(field) || !product[field]) {
-          console.log(`Error: El campo "${field}" es obligatorio.`);
-          return false;
-        }
-      }
-  
-      if (this.products.some(existingProduct => existingProduct.code === product.code)) {
-        console.log(`Error: El código "${product.code}" ya está en uso.`);
-        return false;
-      }
-  
-      return true;
-    }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    getProductById(id) {
-      const product = this.products.find(product => product.code === id);
-  
-      if (product) {
-        return product;
-      } else {
-        console.log('Error: Producto no encontrado.');
-      }
+      return [];
+    } catch (error) {
+      return [];
     }
   }
-  
-  // Ejemplo de uso:
-  const productManager = new ProductManager();
-  
-  const product1 = {
-    title: 'Iphone 13',
-    description: 'Teléfono inteligente de gama alta desarrollado por Apple Inc',
-    price: 1299,
-    thumbnail: 'imagen1.jpg',
-    stock: 46,
-  };
-  
-  const product2 = {
-    title: 'iphone 14',
-    description: 'Igualado en especificaciones con el 13, pero con una mayor autonomía y mejores cámaras',
-    price: 1499,
-    thumbnail: 'imagen2.jpg',
-    stock: 38,
-  };
-  
-  const productCode1 = productManager.addProduct(product1);
-  const productCode2 = productManager.addProduct(product2);
-  
-  console.log('Lista de productos:', productManager.getProducts());
-  console.log('Producto con código 1:', productManager.getProductById(productCode1));
-  console.log('Producto con código 3:', productManager.getProductById(3)); 
-  
+
+  saveProducts() {
+    fs.writeFileSync(this.filePath, JSON.stringify(this.products, null, 2), 'utf8');
+  }
+
+  calculateNextId() {
+    const maxId = this.products.reduce((max, product) => (product.id > max ? product.id : max), 0);
+    return maxId + 1;
+  }
+
+  addProduct(product) {
+    product.id = this.productIdCounter++;
+    this.products.push(product);
+    this.saveProducts();
+    return product.id;
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(id) {
+    const product = this.products.find(p => p.id === id);
+    if (product) {
+      return product;
+    } else {
+      console.log('Error: Producto no encontrado.');
+    }
+  }
+
+  updateProduct(id, newProductData) {
+    const productIndex = this.products.findIndex(p => p.id === id);
+    if (productIndex !== -1) {
+      this.products[productIndex] = { ...this.products[productIndex], ...newProductData };
+      this.saveProducts();
+      return true;
+    } else {
+      console.log('Error: Producto no encontrado.');
+      return false;
+    }
+  }
+
+  deleteProduct(id) {
+    const productIndex = this.products.findIndex(p => p.id === id);
+    if (productIndex !== -1) {
+      this.products.splice(productIndex, 1);
+      this.saveProducts();
+      console.log('Producto eliminado con éxito.');
+    } else {
+      console.log('Error: Producto no encontrado.');
+    }
+  }
+}
+
+const productManager = new ProductManager('products.json');
+
+const product1 = {
+  title: 'Iphone 13',
+  description: 'Teléfono inteligente de gama alta desarrollado por Apple Inc',
+  price: 1299,
+  thumbnail: 'imagen1.jpg',
+  stock: 46,
+};
+
+const product2 = {
+  title: 'iphone 14',
+  description: 'Igualado en especificaciones con el 13, pero con una mayor autonomía y mejores cámaras',
+  price: 1499,
+  thumbnail: 'imagen2.jpg',
+  stock: 38,
+};
+
+const productCode1 = productManager.addProduct(product1);
+const productCode2 = productManager.addProduct(product2);
+
+console.log('Lista de productos:', productManager.getProducts());
+console.log('Producto con código 1:', productManager.getProductById(productCode1));
+console.log('Producto con código 3:', productManager.getProductById(3));
